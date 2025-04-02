f0785d65ad4f91896d8f897cb30c63b13844e842
Ashish <iashishyadav17@gmai.com>
2025-04-02 15:05:33 +0530

    updated

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrpyt = require('bcrypt');
+const bodyParser = require('body-parser');
 const { resolve } = require('path');
+const User = require('./models/User')
+
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
+const dburl = process.env.db_url;
 
+app.use(bodyParser.json());
+
+mongoose.connect(dburl)
+.then(()=> console.log('Mongo DB connected'))
+.catch(err => console.error('MongoDB connection error:', err))
+
+app.post('/register', async(req, res) => {
+  const {username, email, password} = req.body;
+
+  if(!username || !password || !email){
+    return res.status(400).json({success: false, message: 'All fields are required'});
+  }
+  
+  try{
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const newUser = new User({ username, email, password: hashedPassword });
+    
+    await newUser.save();
+    
+    res.status(201).json({success: true, message: 'User registed succesfully'});
+  } catch(error){
+    res.status(500).json({success: false, message: 'Error registering user', error});
+  }
+})
 app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(resolve(__dirname, 'pages/index.html'));
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
